3cde9b49469d92d6f63133176cad90ef0a229eb2
22wito <127059141+22wito@users.noreply.github.com>
2025-05-02 09:41:38 +0200

    Botones

--- src/pruebas_git/PantallaInicial.java
+++ src/pruebas_git/PantallaInicial.java
@@ -6,6 +6,9 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class PantallaInicial extends JFrame {
 
@@ -38,6 +41,27 @@ public class PantallaInicial extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		
+		JButton btnLogin = new JButton("Iniciar sesión");
+		btnLogin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		contentPane.add(btnLogin);
+		
+		JButton btnRegistro = new JButton("Registrarse");
+		btnRegistro.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		contentPane.add(btnRegistro);
+		
+		JButton btnInvitado = new JButton("Entrar como invitado");
+		btnInvitado.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		contentPane.add(btnInvitado);
 	}
 
 }
